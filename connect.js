63009af6fa92174d82d0537d3b3cbc4999bcefe1
Surya Dharmakrishnan <suryad20698@gmail.com>
2022-12-25 20:38:33 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,8 +1,9 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "localhost",
+    host: "containers-us-west-59.railway.app",
     user: "root",
-    password: "1084",
-    database: "social"
+    password: "qUj09HMkk9kOlZfWID6V",
+    database: "railway"
 })
+mysql://root:qUj09HMkk9kOlZfWID6V@containers-us-west-59.railway.app:8065/railway
