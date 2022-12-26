29e822464a8ec0bb39e70172926b89b6bcc2c1f5
Surya Dharmakrishnan <suryad20698@gmail.com>
2022-12-26 10:59:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(Express.json())
 //FOR CROSS ORIGIN(DIFFERENT HOSTS)
 app.use(cors({
-    origin: "https://main--gorgeous-mooncake-5317d9.netlify.app"||"https://gorgeous-mooncake-5317d9.netlify.app"
+    origin: "https://crowdhub.netlify.app"
 }))
 app.use(cookieParser())
 
