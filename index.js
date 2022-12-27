194c4e513904502754cf7b518caabb1b7e334e56
Surya Dharmakrishnan <suryad20698@gmail.com>
2022-12-27 16:28:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,23 @@ import authRoutes from "./routes/auth.js"
 import relationshipRoutes from "./routes/relationships.js"
 import cookieParser from "cookie-parser";
 import cors from "cors"
-import multer from "multer";
+// import multer from "multer";
+import bodyParser from "body-parser";
+import upload from "./multer.js";
+import { uploads } from "./cloudinary.js";
+import fs from 'fs';
+
+
+
 
 //MIDDLEWARES
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true)
     next()
 })
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 //TO SEND JSON OBJECTS
 app.use(Express.json())
 //FOR CROSS ORIGIN(DIFFERENT HOSTS)
@@ -25,21 +35,21 @@ app.use(cookieParser())
 
 
 //IMAGE UPLOAD MANAGEMENT AND ITS NAMING
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, '../frontend/public/upload')
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + file.originalname)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, '../frontend/public/upload')
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + file.originalname)
+//     }
+// })
 
-const upload = multer({ storage: storage })
+// const upload = multer({ storage: storage })
 
-app.post("/api/upload", upload.single("file"), (req, res) => {
-    const file = req.file;
-    res.status(200).json(file.filename);
-})
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//     const file = req.file;
+//     res.status(200).json(file.filename);
+// })
 
 //GETTING ALL THE ENDPOINTS(ROUTES)
 app.use("/api/users", userRoutes)
@@ -48,6 +58,29 @@ app.use("/api/comments", commentRoutes)
 app.use("/api/likes", likeRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/relationships", relationshipRoutes)
+app.use("/api/upload", upload.array('file'), async (req, res) => {
+    const uploader = async (path) => await uploads(path, "Images")
+    if (req.method === 'POST') {
+        const urls = []
+        const files = req.files
+        for (const file of files) {
+            const { path } = file
+            const newPath = await uploader(path)
+            urls.push(newPath)
+            fs.unlinkSync(path)
+        }
+        res.status(200).json({
+            message: "Image uploaded successfully",
+            data: urls
+        })
+    } else {
+        res.status(405).json({
+            err: "Image is not uploaded"
+        })
+    }
+})
+
+
 
 const PORT=8800;
 //STARTING THE BACKEND SERVER
